eb45a2ee1edc23a689cca909315ac80ac92b2438
Ruslan Albu <ruslanalbu@page.com>
2016-03-10 12:08:07 +0000

    performance samle added

--- NLogExtension/Controllers/HomeController.cs
+++ NLogExtension/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Diagnostics;
+using System.Threading;
 using System.Web.Mvc;
 using NLog;
 using NLogExtension.Extensions;
@@ -15,9 +13,35 @@ namespace NLogExtension.Controllers
         public ActionResult Index()
         {
             Logger.ActionInit("HomeController Index");
+
+            GetUsers();
+
+            GetActiveItems();
+
             Logger.Debug("Sample debug message");
 
             return View();
+        }
+
+        private void GetUsers()
+        {
+            Logger.Debug("Get all users");
+
+            Stopwatch sw = Stopwatch.StartNew();
+            Thread.Sleep(700);
+            sw.Stop();
+
+            Logger.Performance($"Getting users in: {sw.ElapsedMilliseconds}");
+        }
+
+        private void GetActiveItems()
+        {
+            Logger.Debug("Searching for active items");
+
+            Stopwatch sw = Stopwatch.StartNew();
+            Thread.Sleep(300);
+            sw.Stop();
+            Logger.Performance($"Searching active items in: {sw.ElapsedMilliseconds}");
         }
 
         public ActionResult About()
---
+++ NLogExtension/Extensions/LoggerExtension.cs
@@ -0,0 +1,16 @@
+﻿using NLog;
+
+namespace NLogExtension.Extensions
+{
+    public static class LoggerExtension
+    {
+        public static void ActionInit(this ILogger logger, string message)
+        {
+            logger.Debug($"ACTION-001 {message}");
+        }
+        public static void Performance(this ILogger logger, string message)
+        {
+            logger.Debug($"PERFORMANCE-001 {message}");
+        }
+    }
+}
